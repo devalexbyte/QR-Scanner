c9e17a492294d3161701b1a29c987fabd0a15ce1
p0m0n <p0m0n@ukr.net>
2022-12-21 12:24:11 +0200

    Enable: View Binding
    https://developer.android.com/topic/libraries/view-binding

--- app/src/main/java/com/devalexbyte/qrscanner/MainActivity.kt
+++ app/src/main/java/com/devalexbyte/qrscanner/MainActivity.kt
@@ -19,12 +19,16 @@ package com.devalexbyte.qrscanner
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.devalexbyte.qrscanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?)
     {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
